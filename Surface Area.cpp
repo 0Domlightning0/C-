3787db713228a5150e896c28cceca524184eb570
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-09-06 18:15:03 -0700

    Create Surface Area.cpp

---
+++ Surface Area.cpp
@@ -0,0 +1,17 @@
+//Dominik Wrobel
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int legnth, width;
+    cout << "What is the legenth \n";
+    cin >> legnth;
+    cout << "What is the width \n";
+    cin >> width;
+    cout << "The Surface Area is " << (width * 2) + (legnth * 2);
+    return 0;
+}
+
