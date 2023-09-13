f1e0038c8c103c48daf9dd829dbcd88b35ee348b
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-09-13 11:34:36 -0400

    Create Pay.cpp

---
+++ Pay.cpp
@@ -0,0 +1,29 @@
+// Dominik Wrobel
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double regularWages,
+		basePayRate = 16,   // My hourly summer camp wage
+		regularHours = 40;  // My hours over a week 8*5
+
+	/*
+	Hello
+	This
+	Is 
+	Dominik,
+	This
+	Is
+	Cool
+	*/
+
+	// Calculates the amount of money I get over one week
+	regularWages = basePayRate * regularHours;
+
+	// Display the total money for a week
+	cout << "Wages for this week are " << regularWages << "$ \n ";
+	return 0;
+
+
+}
