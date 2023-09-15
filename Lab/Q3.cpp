5df789da6447f37bd1cb320664b1c2405008dbfa
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-09-15 10:21:25 -0400

    Create Q3.cpp

---
+++ Lab/Q3.cpp
@@ -0,0 +1,29 @@
+
+//Dominik Wrobel - 400508446
+
+// Lab1 - Dominik Wrobel.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+// Question 2
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+	// ans = answer, num = number
+	int num, ans;
+   // Prompt
+	cout << "Please input an integer number ";
+	cin >> ans;
+
+	//Space
+	cout << "\n";
+
+	cout << "1. Number divisible by 3? \n \n" << "2. Number between 10 and 100 ?\n \n ";
+
+	if (ans == 1);
+
+
+		
+}
