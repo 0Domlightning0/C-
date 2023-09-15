e7d3897ca4130a5ab006224e1529458f614977ea
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-09-15 10:08:11 -0400

    Create Q1.cpp

---
+++ Lab/Q1.cpp
@@ -0,0 +1,22 @@
+// Lab1 - Dominik Wrobel.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Question 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   // Variable Assignment
+    string First_Name = "Dominik";
+    int Age = 17;
+    double Hourly_Wage = 16.00;
+    char Gender = 'M';
+
+
+    // Printing out to console in 4 seperate lines with a semicolon infront of the text and a space seperating the variable
+    cout << "First Name: " << First_Name << "\n";
+    cout << "Age: " << Age << "\n";
+    cout << "Hourly Wage: " << Hourly_Wage << "\n";
+    cout << "Gender: " << Gender;
+
+}
