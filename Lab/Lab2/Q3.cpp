748ce83262f60975561786cc5771f300978f4da6
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-10-06 10:08:12 -0400

    Update Q3.cpp

--- Lab/Lab2/Q3.cpp
+++ Lab/Lab2/Q3.cpp
@@ -1,48 +1,67 @@
-// Lab3_Q3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Lab3 - Q3
 
+// Library Assignment
 #include <iostream>
 #include <cstdlib>
 #include <stdlib.h>
 
 using namespace std;
 
-void FindClosest(int In1, int In2, int In3) {
-    if (In1 == 0 and In2 == 0 and In3 == 0) {
-        cout << "Code finished";
-        exit(0);
-
-    }
-    if (abs(In1) <= abs(In2) and abs(In1) <= abs(In3)) {
-        cout << "the lowest number is " << In1 << endl << endl;
-
-    }
-    else if (abs(In2) <= abs(In3) and abs(In2) <= abs(In1)) {
-        cout << "the lowest number is " << In2 << endl << endl;
-    }
-    else if (abs(In3) <= abs(In1) and abs(In3) <= abs(In2)) {
-        cout << "the lowest number is " << In3 << endl << endl;
-    }
-    
-}
+void FindClosest(int In1, int In2, int In3);
 
 int main() {
     int Yes = 1, In1, In2, In3;
     while (Yes == 1) {
-        cout << " Please give the fist number" << endl;
+        cout << " Please give the first number" << endl;
         cin >> In1;
         cout << " Please give the second number" << endl;
         cin >> In2;
         cout << " Please give the third number" << endl;
         cin >> In3;
         FindClosest(In1, In2, In3);
-        
+
 
 
 
     }
-    
-    
+
+
+
+}
+
+
+// Function to Find the number closest to 0, it will prioritize the number that comes first
+void FindClosest(int In1, int In2, int In3) {
+
+    // If all the numbers are == 0, the entire code closes
+    if (In1 == 0 and In2 == 0 and In3 == 0) {
+        cout << "Code finished";
+        exit(0);
+
+    }
+
+
+    // If the first input is less than or equal to the 2nd and 3rd, it will print out the answer
+    if (abs(In1) <= abs(In2) and abs(In1) <= abs(In3)) {
+        cout << "the lowest number is " << In1 << endl << endl;
+
+    }
+
+    /*If the second input is less than or equal to the 1st and 3rd, it will print out the answer
+    This will only run if the first argument isnt true
+    */
+    else if (abs(In2) <= abs(In3) and abs(In2) <= abs(In1)) {
+        cout << "the lowest number is " << In2 << endl << endl;
+    }
+
+
+
+    /*If the third input is less than or equal to the 1st and 2nd, it will print out the answer
+    This will only run if the first and second arguments arent true
+    */
+    else if (abs(In3) <= abs(In1) and abs(In3) <= abs(In2)) {
+        cout << "the lowest number is " << In3 << endl << endl;
+    }
 
 }
 
@@ -56,23 +75,3 @@ int main() {
 
 
 
-
-/*#include <iostream>
-
-using namespace std;
-
-void displaymessage();
-
-int main()
-{
-    
-    displaymessage();
-}
-
-void displaymessage(){
-
-    cout << "Hello from Display Function";
-
-
-}
-*/
