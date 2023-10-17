2f7b136e9f07d628e82e35732dab516ab8468856
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-10-16 20:53:23 -0700

    Update Underline-Q1.cpp

--- Lab/Lab2/Underline-Q1.cpp
+++ Lab/Lab2/Underline-Q1.cpp
@@ -1,1 +1,36 @@
+// Dominik Wrobel - 400508446
+// Lab3-Q1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+
+using namespace std;
+
+string Underline(string Input);
+
+int main()
+{
+    string Title;
+    cout << "Please Input your Title: ";
+    getline(cin,Title);
+    Underline(Title);
+    cout << endl;
+    exit(0);
+}
+
+string Underline(string Input) {
+    double len;
+    string line = "";
+    len = Input.length();
+
+    for (int i = 0; i < len; i++) {
+        line += "=";
+
+    }
+    cout << Input << endl << line;
+    return 0;
+
+}
