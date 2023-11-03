2c8d07ff1ab7ba1b5676aa357aaf147a49bc7b66
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-11-03 09:10:08 -0400

    Create Q1-HundredRand.cpp

---
+++ Lab/Lab5/Q1-HundredRand.cpp
@@ -0,0 +1,52 @@
+// Lab5-Q1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double Max(double array[]);
+
+int main()
+{
+    int random, top, bot;
+
+    double array[100] = {};
+
+    double Total = 0;
+
+    for (int i = 0; i < 99; i++) {
+
+        bot = 100;
+        top = 200;
+
+
+        random = bot + rand() % (top + 1 - bot);
+
+        array[i] = random;
+
+        cout << array[i] << " ";
+
+        Total += random;
+
+
+
+   }
+    cout << endl << endl << Total/100; 
+
+    Max(array);
+}
+
+double Max(double array[100]) {
+    double pos = 0, val, topval = 0;
+    for (int i = 0; i < 99; i++) {
+        val = array[i];
+        if (val > topval) {
+            topval = val;
+            pos = i;
+        }
+    }
+    cout << endl << topval << " " << pos;
+    return 0;
+}
