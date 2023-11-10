dfe2da5511c6a503fe7e871d4c0975039d92a9af
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-11-10 09:40:07 -0500

    Update Array9x9.cpp

--- Assignments/Assignment4/Array9x9.cpp
+++ Assignments/Assignment4/Array9x9.cpp
@@ -12,7 +12,8 @@ int main()
 {
     // variable Assessmant
     int min = 0;
-    int max = 9;
+    int max = 2;
+    int num;
 
     // Creates a seed using the current time
     srand((unsigned)time(NULL));
@@ -24,31 +25,52 @@ int main()
 
             
 
-            int Random = min + (rand() % 8);
+            int Random = min + (rand() % max);
 
             Array[i][j] = Random;
             
 
         }
     }
+
+    cout << "What is your number?";
+
+    cin >> num;
     
     // For every Row
     for (int i = 0; i < 9; i++) {
         //For every Coloumn 
-        for (int j = 0; j < 5; j++) {
-
-            if (Array[i][j] == Array[i][j + 1] and Array[i][j] == Array[i][j + 2] and Array[i][j] == Array[i][j + 3]) {
-                cout << "yes";
-
-                {
+        for (int j = 0; j < 6; j++) {
+            if (Array[i][j] == num and Array[i][j] == Array[i][j + 1] and Array[i][j] == Array[i][j + 2] and Array[i][j] == Array[i][j + 3]) {
+                cout << "yes there are 4 numbers horizonatlly" << endl;
 
 
 
 
-                }
+
+
+
             }
 
+        }
+    }
 
+    for (int i = 0; i < 9; i++) {
+        //For every Coloumn 
+        for (int j = 0; j < 5; j++) {
+            if (Array[i][j] == num and Array[j][i + 1] == num and Array[j][i] == Array[j][i + 2] and Array[i][j] == Array[i][j + 3] ) {
+                cout << "yes there are 4 numbers Vertically" << endl;
+
+
+
+
+
+
+
+            }
+
+        }
+    }
 
 
         for (int i = 0; i < 9; i++) {
@@ -56,11 +78,12 @@ int main()
             for (int j = 0; j < 9; j++) {
 
 
-               cout << Array[i][j] << " ";
+            cout << Array[i][j] << " ";
 
-             }
-
-        }
-
+            }
 
         }
+
+
+}
+   
