347a976468e46775c8e294afe914cc1eee7b798b
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-11-12 11:33:05 -0800

    Update Q1-Lockers100.cpp

--- Assignments/Assignment4/Q1-Lockers100.cpp
+++ Assignments/Assignment4/Q1-Lockers100.cpp
@@ -1,15 +1,10 @@
 // Dominik Wrobel - 400508446
-// Assignment4 - Q1 (Alot of Comments)
+// Assignment4 - Q1 
 
 // Library Assignment
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <cmath>
-#include <tuple>
-using namespace std;
 
+using namespace std;
 
 int Lockers[100] = {};
 int students = 100;
@@ -20,32 +15,39 @@ int main() {
     // LowLim =  Lower Limit, UpLim = Upper Limit
     int LowLim = 0;
     int UpLim = 100;
+
+    // Creates an array of 100 closed lockers
     for (int i = 0; i < 101; i++) {
         Lockers[i] = false;
-        cout << "done creating" << endl;
+
     }
 
     for (int i = 1; i < 101; i++) {
-        cout << "did it" << endl;
-        for (int j = i; j < 101; j+i){
-            
-            if (Lockers[j-1] == 1) {
-                cout << "Now false  " << j << endl;
-                Lockers[j-1] = 0;
+
+        for (int j = i; j < 101; j + i) {
+
+            if (Lockers[j - 1] == 1) {
+
+                Lockers[j - 1] = 0;
                 j += i;
-              
-                
+
+
             }
 
-            else if (Lockers[j-1] == 0) {
-                cout << "Now true  " << j << endl;
-                Lockers[j-1] = 1;
+            else if (Lockers[j - 1] == 0) {
+
+                Lockers[j - 1] = 1;
                 j += i;
             }
         }
     }
 
+    cout << "The lockers that are still open are: ";
+
     for (int i = 0; i < 100; i++) {
-        cout << i+1 << " " << Lockers[i] << "  ";
+        if (Lockers[i] == 1) {
+            cout << i+1 << " ";
+        }
     }
+    cout << endl;
 }
