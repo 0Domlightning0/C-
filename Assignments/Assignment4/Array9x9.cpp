122698a71eeb6d007f6efe677d84ad5bc2b3194d
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-11-12 11:05:01 -0800

    Update Array9x9.cpp

--- Assignments/Assignment4/Array9x9.cpp
+++ Assignments/Assignment4/Array9x9.cpp
@@ -8,82 +8,100 @@ using namespace std;
 // Creating a 9x9
 int Array[9][9] = {};
 
+bool Is4InARow(int[9][9], int num);
+
 int main()
 {
     // variable Assessmant
+    // Min is the smallest number avalable to be generated
     int min = 0;
-    int max = 2;
+    // The amount of numbers that can be produced ex 0-9 is 10 numbers
+    int range = 2;
+    // The number that is being inputted and checked
     int num;
+    
 
     // Creates a seed using the current time
     srand((unsigned)time(NULL));
 
-    // For every Row
+    // For every Row (9 rows)
     for (int i = 0; i < 9; i++) {
-        //For every Coloumn 
+        //For every Coloumn (9 columns)
         for (int j = 0; j < 9; j++) {
 
-            
 
-            int Random = min + (rand() % max);
+            // Creating the array
+            int Random = min + (rand() % range);
 
             Array[i][j] = Random;
-            
+
 
         }
     }
 
-    cout << "What is your number?";
+    cout << "What is your number? ";
 
     cin >> num;
-    
-    // For every Row
-    for (int i = 0; i < 9; i++) {
-        //For every Coloumn 
-        for (int j = 0; j < 6; j++) {
-            if (Array[i][j] == num and Array[i][j] == Array[i][j + 1] and Array[i][j] == Array[i][j + 2] and Array[i][j] == Array[i][j + 3]) {
-                cout << "yes there are 4 numbers horizonatlly" << endl;
 
+    if (Is4InARow(Array, num) == true) {
+        cout << "There are 4 Numbers in a line" << endl;
 
-
-
-
-
-
-            }
-
-        }
     }
-
-    for (int i = 0; i < 9; i++) {
-        //For every Coloumn 
-        for (int j = 0; j < 5; j++) {
-            if (Array[i][j] == num and Array[j][i + 1] == num and Array[j][i] == Array[j][i + 2] and Array[i][j] == Array[i][j + 3] ) {
-                cout << "yes there are 4 numbers Vertically" << endl;
-
-
-
-
-
-
-
-            }
-
-        }
+    else {
+        cout << "There are not 4 Numbers in a line" << endl;
     }
 
 
-        for (int i = 0; i < 9; i++) {
-            cout << endl;
-            for (int j = 0; j < 9; j++) {
+
+
+    for (int i = 0; i < 9; i++) {
+        cout << endl;
+        for (int j = 0; j < 9; j++) {
 
 
             cout << Array[i][j] << " ";
 
-            }
-
         }
+
+    }
 
 
 }
-   
+
+bool Is4InARow(int[9][9], int num) {
+    // For every Row
+    for (int i = 0; i < 9; i++) {
+        //For every Coloumn 
+        for (int j = 0; j < 6; j++) {
+            // Checks if the current number and the three after are the same ansd equal to num
+            if (Array[i][j] == num and Array[i][j] == Array[i][j + 1] and Array[i][j] == Array[i][j + 2] and Array[i][j] == Array[i][j + 3]) {
+                return true;
+
+
+
+
+
+
+
+            }
+
+        }
+    }
+    //for every Row
+    for (int i = 0; i < 9; i++) {
+        //For every Coloumn 
+        for (int j = 0; j < 5; j++) {
+            // Checks if the current number and the three below
+            if (Array[i][j] == num and Array[j][i + 1] == num and Array[j][i] == Array[j][i + 2] and Array[i][j] == Array[i][j + 3]) {
+                return true;
+
+
+
+
+
+
+            }
+
+        }
+    }
+}
