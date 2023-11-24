c3900d60c9f44fb1b9d2756228d6e6aa416b0e3b
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-11-24 15:16:15 -0800

    Update Lab62.cpp

--- Lab/Lab6/Lab62.cpp
+++ Lab/Lab6/Lab62.cpp
@@ -1,42 +1,68 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
+#include <fstream>
 using namespace std;
 
-string DNAtoRNA(string DNA);
+//prototypes
+string DNAtoRNA(string, string);
+
+string countDNAnt(string[20]);
 
 int main()
 {
+	// Array Creation
+	string array[20] = {};
+
+
+	string RNA = "";
+
 	string DNA;
 
-	// Array Assignment 
 	
+
 
 	// Reading the file
 	fstream outfile("DNAdata.txt", ios::in);
 
-
+	// Opens the File for input
+	ofstream file_enter;
+	file_enter.open("RNAdata");
 
 	// Creates the Array using the file
 	for (int i = 0; i < 20; i++) {
-
-		outfile >> DNA;
-		cout << DNA << endl << endl;
+		outfile >> array[i];
+		//cout << array[i] << endl;
+			
+		
 	}
-
 	
-
-
+	// Runs the code to convert one line of DNA to RNA 20 times to complete the entire file
+	for (int i = 0; i < 20; i++) {
+		cout << DNAtoRNA(array[i], RNA) << endl;
+		file_enter << DNAtoRNA(array[i], RNA) << endl;
+	}
 }
 
-string DNAtoRNA(string DNA) {
-	for (int i = 0; i < 20; i++) {
-		if (DNA[i] == 'T') {
-			DNA[i] = 'U';
+// Takes one line from the DNA array and an empty string to insert the RNA string into
+string DNAtoRNA(string array,string RNA) {
+	string DNA = "";
+	DNA = array;
+	for (int j = 0; j < 100; j++) {
+		// If the letter it found is equal to T
+		if (DNA[j] == 'T') {
+			// The program will convert it into a U
+				DNA[j] = 'U';
 		}
-		cout << DNA;
+			
 	}
+	RNA = DNA;
+	return RNA;
+}
+
+
+
+string countDNAnt(string[20]){
 	return 0;
 }
-
-
