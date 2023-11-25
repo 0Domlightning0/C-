2b080f5cae5313bb2166704c29bce6a7c48c8029
Domlightning <99225898+0Domlightning0@users.noreply.github.com>
2023-11-24 18:19:44 -0800

    Update and rename Lab62.cpp to Lab6-DNAtoRNA.cpp

--- Lab/Lab6/Lab6-DNAtoRNA.cpp
+++ Lab/Lab6/Lab6-DNAtoRNA.cpp
@@ -1,8 +1,11 @@
+//Dominik Wrobel - 400508446
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <string.h>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
 //prototypes
@@ -21,7 +24,7 @@ int main()
 
 	int NumA = 0, NumT = 0, NumC = 0, NumG = 0;
 
-	
+
 
 
 	// Reading the file
@@ -35,31 +38,40 @@ int main()
 	for (int i = 0; i < 20; i++) {
 		outfile >> array[i];
 		//cout << array[i] << endl;
-			
-		
+
+
 	}
-	
+
+
+	cout << "Total DNA for each strand:" << endl;
+
+	cout << "#  " << "A   " << "C   " << "G   " << "T   " << endl;
 	// Runs the code to convert one line of DNA to RNA 20 times to complete the entire file
-	for (int i = 0; i < 20; i++) {
-		
-		
+	for (int i = 1; i < 21; i++) {
+
+
 		//cout << DNAtoRNA(array[i], RNA) << endl;
-		file_enter << DNAtoRNA(array[i], RNA) << endl;
-		cout << countDNAnt(array[0], &NumA, &NumT, &NumC, &NumG);
+		file_enter << DNAtoRNA(array[i - 1], RNA) << endl;
+		countDNAnt(array[i - 1], &NumA, &NumT, &NumC, &NumG);
+
+		cout << left << setw(3);
+		cout << left << setw(3) << i << setw(4) << left << NumA << setw(4) << left << NumC << setw(3) << left << NumG << right << setw(3) << NumT << endl;
+
 	}
+	return 0;
 }
 
 // Takes one line from the DNA array and an empty string to insert the RNA string into
-string DNAtoRNA(string array,string RNA) {
+string DNAtoRNA(string array, string RNA) {
 	string DNA = "";
 	DNA = array;
 	for (int j = 0; j < 100; j++) {
 		// If the letter it found is equal to T
 		if (DNA[j] == 'T') {
 			// The program will convert it into a U
-				DNA[j] = 'U';
+			DNA[j] = 'U';
 		}
-			
+
 	}
 	RNA = DNA;
 	return RNA;
@@ -67,23 +79,29 @@ string DNAtoRNA(string array,string RNA) {
 
 
 
-string countDNAnt(string DNA, int* NumA, int* NumT, int* NumC, int* NumG){
+string countDNAnt(string DNA, int* NumA, int* NumT, int* NumC, int* NumG) {
+	int NumofA = 0, NumofT = 0, NumofC = 0, NumofG = 0;
 	for (int j = 0; j < 100; j++) {
 		if (DNA[j] == 'T') {
-			*NumT += 1;
+			NumofT += 1;
 		}
 		if (DNA[j] == 'A') {
-			*NumA += 1;
+			NumofA += 1;
 		}
 		if (DNA[j] == 'C') {
-			*NumC += 1;
+			NumofC += 1;
 		}
 		if (DNA[j] == 'G') {
-			*NumG += 1;
+			NumofG += 1;
 		}
 	}
 
-	
-	return 0;
+	*NumA = NumofA;
+	*NumT = NumofT;
+	*NumC = NumofC;
+	*NumG = NumofG;
+
+
+	return "yes";
 }
 
